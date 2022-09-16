0e6e47bd172f4872ca0db2b2286c21a6838cfaba
Ludo Hund <Ludovic_hund@hotmail.fr>
2022-09-16 19:21:09 +0200

    train

---
+++ typereact/src/components/Message.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { MessagesInt } from '../model';
+
+type Props = {
+    mess: MessagesInt;
+    messData: MessagesInt[];
+    setMessData: React.Dispatch<React.SetStateAction<MessagesInt[]>>
+}
+
+const Message = ({mess, messData, setMessData}: Props) => {
+
+    const dateFormater = (date: number) => {
+        return new Date(date).toLocaleDateString('fr-FR', {day: 'numeric', month: 'long'});
+    }
+
+    const handleDelete = ()=>{
+        setMessData(messData.filter((element)=>element.id !== mess.id));
+    }
+
+    return (
+        <div className="message-container">
+            <p>{mess.message}</p>
+            <h5>{dateFormater(mess.date)}</h5>
+            <span id='delete' onClick={()=>handleDelete()}>&#10008;</span>
+        </div>
+    );
+};
+
+export default Message;
